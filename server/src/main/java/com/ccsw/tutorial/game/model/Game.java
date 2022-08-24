c1c077ffd6973cf891eb440732536e35775eba9a
AlejandroMarquino <alejandromarquino@gmail.com>
2022-08-24 12:07:36 +0200

    cambios data.sql

--- server/src/main/java/com/ccsw/tutorial/game/model/Game.java
+++ server/src/main/java/com/ccsw/tutorial/game/model/Game.java
@@ -12,8 +12,9 @@ import javax.persistence.Table;
 import com.ccsw.tutorial.author.model.Author;
 import com.ccsw.tutorial.category.model.Category;
 
-//@author AMF 
-
+/**
+ * @author ccsw
+ */
 @Entity
 @Table(name = "Game")
 public class Game {
@@ -37,71 +38,81 @@ public class Game {
     @JoinColumn(name = "author_id", nullable = false)
     private Author author;
 
-    // @return id
-
+    /**
+     * @return id
+     */
     public Long getId() {
 
         return this.id;
     }
 
-    // @param id new value of {@link #getId}.
-
+    /**
+     * @param id new value of {@link #getId}.
+     */
     public void setId(Long id) {
 
         this.id = id;
     }
 
-    // @return title
-
+    /**
+     * @return title
+     */
     public String getTitle() {
 
         return this.title;
     }
 
-    // @param title new value of {@link #getTitle}.
-
+    /**
+     * @param title new value of {@link #getTitle}.
+     */
     public void setTitle(String title) {
 
         this.title = title;
     }
 
-    // @return age
-
+    /**
+     * @return age
+     */
     public String getAge() {
 
         return this.age;
     }
 
-    // @param age new value of {@link #getAge}.
-
+    /**
+     * @param age new value of {@link #getAge}.
+     */
     public void setAge(String age) {
 
         this.age = age;
     }
 
-    // @return category
-
+    /**
+     * @return category
+     */
     public Category getCategory() {
 
         return this.category;
     }
 
-    // @param category new value of {@link #getCategory}.
-
+    /**
+     * @param category new value of {@link #getCategory}.
+     */
     public void setCategory(Category category) {
 
         this.category = category;
     }
 
-    // @return author
-
+    /**
+     * @return author
+     */
     public Author getAuthor() {
 
         return this.author;
     }
 
-    // @param author new value of {@link #getAuthor}.
-
+    /**
+     * @param author new value of {@link #getAuthor}.
+     */
     public void setAuthor(Author author) {
 
         this.author = author;
